ac227c6012f3924ed856c99cdce3d589dbcb53b1
boociil <ryanardiansyah208@gmail.com>
2024-11-21 12:58:12 +0700

    setting routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css'
-import App from './App.jsx'
+import { AuthProvider } from './components/AuthContext.jsx'; // Pastikan ini benar
+import RoutesAdmin from './components/RoutesAdmin.jsx';
 import Main from './components/Main.jsx'
 import Footer from './components/Footer.jsx'
 import Header from './components/Header.jsx'
@@ -12,34 +13,40 @@ import AdminPage from "./components/AdminPage.jsx"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route index element={
-          <>
-            <Header type="fixed"/>
-            <Main />
-            <Footer />
-          </>
-        }></Route>
-        <Route path="/Login" element={
-          <>
-            <Header />
-            <Login />
-          </>
-        }></Route>
-        <Route path="/Form-keg/:id" element={
-          <>
-            <Header txt="text-black" type="fixed" />
-            <Form />
-          </>
-        }></Route>
-        <Route path="/Admin/" element={
-          <>
-            <Header type="fixed" />
-            <AdminPage />
-          </>
-        }></Route>
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider> 
+      <BrowserRouter>
+        <Routes>
+          <Route index element={
+            <>
+              <Header type="fixed"/>
+              <Main />
+              <Footer />
+            </>
+          }></Route>
+          <Route path="/Login" element={
+            <>
+              <Header />
+              <Login />
+            </>
+          }></Route>
+          <Route element={<RoutesAdmin />}>
+            <Route path="/Admin/" element={
+              <>
+                <Header type="fixed" />
+                <AdminPage />
+              </>
+            }></Route>
+          </Route>
+
+          <Route path="/Form-keg/:id" element={
+            <>
+              <Header txt="text-black" type="fixed" />
+              <Form />
+            </>
+          }></Route>
+          
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   </StrictMode>,
 )
